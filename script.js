70599b801cd7c02fa7dad3db78903faa5e8e4d5a
michael <87037066+shoemakersknife@users.noreply.github.com>
2025-10-10 14:32:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("page loaded1");
   setTimeout(() => {
     const heading = document.querySelector("h1");
     heading.style.display = "block";
-  }, 1000); // 1000 milliseconds = 1 second
+  }, 500);
 });
