614b42c4f86d7003f1dfb7b3ee25421ee198922d
michael <87037066+shoemakersknife@users.noreply.github.com>
2025-10-10 14:21:24 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    const heading = document.querySelector("h1");
+    heading.style.display = "block";
+  }, 1000); // 1000 milliseconds = 1 second
+});
