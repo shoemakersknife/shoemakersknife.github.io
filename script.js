bc2081e0bcd69b7704e580398cdb897c095aea24
michael <87037066+shoemakersknife@users.noreply.github.com>
2025-10-10 14:52:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     const heading = document.querySelector("h1");
-    heading.style.display = "block";
-    
-     setTimeout(() => {
-    const paragraph = document.querySelector("p");
-    paragraph.style.display = "block";
-     }, 500);
+    heading.classList.remove("hidden");
+    heading.classList.add("visible");
+
+    setTimeout(() => {
+      const paragraph = document.querySelector("p");
+      paragraph.classList.remove("hidden");
+      paragraph.classList.add("visible");
+    }, 500);
   }, 500);
 });
